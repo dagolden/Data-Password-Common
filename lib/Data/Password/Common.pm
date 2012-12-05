6d8b724db0e5bd4ec62f2c23a8a383eafdf4b005
David Golden <dagolden@cpan.org>
2012-12-05 12:55:47 -0500

    fix and rename subroutine; add docs'

--- lib/Data/Password/Common.pm
+++ lib/Data/Password/Common.pm
@@ -12,16 +12,20 @@ use IO::File;
 use Search::Dict;
 use autodie 2.00;
 
-use Sub::Exporter -setup => { exports => ['check'] };
+use Sub::Exporter -setup => { exports => ['found'] };
 
-my $list_path = File::ShareDir::dist_file( "Data-Password-Common", "common.txt" );
+my $list_path = File::ShareDir::dist_file( "Data-Password-Common", "common.txt" )
+  or die "Can't locate common passwords file";
 
 my $list_handle = IO::File->new($list_path);
 
-sub check {
+sub found {
   return unless @_;
   my $password = shift;
-  return -1 != look $list_handle, $password;
+
+  look $list_handle, $password;
+  chomp( my $found = <$list_handle> );
+  return $found eq $password;
 }
 
 1;
@@ -30,20 +34,39 @@ sub check {
 
 =head1 SYNOPSIS
 
-  use Data::Password::Common;
+  use Data::Password::Common 'found';
+
+  if ( found( $password ) ) {
+    die "'$password' is a common password"
+  }
+
+  # import with aliasing
+  use Data::Password::Common found => { -as => "found_common" };
 
 =head1 DESCRIPTION
 
-This module might be cool, but you'd never know it from the lack
-of documentation.
+This module installs a list of 62 thousand common passwords and provides
+a function to check a string against the list.
+
+The password list from InfoSecDaily
+at L<http://www.isdpodcast.com/resources/62k-common-passwords/>
 
 =head1 USAGE
 
-Good luck!
+Functions are provided via L<Sub::Exporter>.  Nothing is exported by default.
+
+=head2 found
+
+  found($password);
+
+Returns true if the password is in the common passwords list.
 
 =head1 SEE ALSO
 
-Maybe other modules do related things.
+=for :list
+* L<Data::Password>
+* L<Data::Password::Entropy>
+* L<Data::Password::BasicCheck>
 
 =cut
 
--- t/lookup.t
+++ t/lookup.t
@@ -6,10 +6,10 @@ use Test::File::ShareDir -share => {
   -dist => { 'Data-Password-Common' => 'share' }
 };
 
-use Data::Password::Common qw/check/;
+use Data::Password::Common qw/found/;
 
-ok( check("password"), "'password' is a common password" );
-ok( check("alkdjf1=2"), "'alkdjf1=2' is not a common password" );
+ok( found("password"), "'password' is a common password" );
+ok( ! found("alkdjf1=2"), "'alkdjf1=2' is not a common password" );
 
 done_testing;
 # COPYRIGHT
